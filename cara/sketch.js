5d26044eb8c7c852e0a8fea432b64d4e9728b983
mszabo4 <mszabo4@iespompeufabra.cat>
2024-09-17 14:28:56 +0200

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -1,7 +1,11 @@
 function setup() {
+ createCanvas(600,400);
  // put setup code here
 }
 
 function draw() {
-  // put drawing code here
+ background(127);
+ fill(255,0,0);
+ ellipse(300,200,220,300);
+        // put drawing code here
 }
