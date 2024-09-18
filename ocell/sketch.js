01f92f1772563cdd616e613844a8ecbb9b3a479b
mszabo4 <mszabo4@iespompeufabra.cat>
2024-09-18 11:15:29 +0200

    Update sketch.js

--- ocell/sketch.js
+++ ocell/sketch.js
@@ -1,7 +1,36 @@
 function setup() {
- // put setup code here
+ createCanvas(400,120);
 }
 
 function draw() {
-  // put drawing code here
+  background(294)
+ //ocell esquerra
+ beginShape();
+ vertex(50,120);
+ vertex(100,90);
+ vertex(110,60);
+ vertex(80,20);
+ vertex(210,60);
+ vertex(160,80);
+ vertex(200,90);
+ vertex(140,100);
+ vertex(130,120);
+ endShape();
+ fill(0);
+ ellipse(155,60,8,8);
+
+ //ocell dreta
+  beginShape();
+ vertex(50,120);
+ vertex(100,90);
+ vertex(110,60);
+ vertex(80,20);
+ vertex(210,60);
+ vertex(160,80);
+ vertex(200,90);
+ vertex(140,100);
+ vertex(130,120);
+ endShape();
+ fill(0);
+ ellipse(155,60,8,8);
 }
